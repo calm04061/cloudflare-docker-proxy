866ee17622697332d882fd3e93bbab8af46ea9a0
han <calm0406@163.com>
2023-08-08 22:55:15 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,13 +4,13 @@ addEventListener("fetch", (event) => {
 });
 
 const routes = {
-  "docker.libcuda.so": "https://registry-1.docker.io",
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
+  "docker.calm0406.tk": "https://registry-1.docker.io",
+  "quay.calm0406.tk": "https://quay.io",
+  "gcr.calm0406.tk": "https://gcr.io",
+  "k8s-gcr.calm0406.tk": "https://k8s.gcr.io",
+  "k8s.calm0406.tk": "https://registry.k8s.io",
+  "ghcr.calm0406.tk": "https://ghcr.io",
+  "cloudsmith.calm0406.tk": "https://docker.cloudsmith.io",
 };
 
 function routeByHosts(host) {
